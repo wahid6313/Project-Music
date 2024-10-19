71dea3bc58628c47ddda3c2fba2e61eaaa074011
Wahid Ali <wahidali8566@gmail.com>
2024-10-19 15:10:41 +0530

    layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>Navbar</h1>
         {children}
       </body>
     </html>
---
+++ app/music/page.tsx
@@ -0,0 +1,3 @@
+export default function firstPage() {
+  return <h1>this is my first page</h1>;
+}
