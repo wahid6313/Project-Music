547241580b99e78971b198fcef0f5023cae7bc7a
Wahid Ali <wahidali8566@gmail.com>
2024-10-20 23:01:35 +0530

    cards-section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
 
 import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
     <div className="  w-full  text-black overflow-hidden ">
+      <Spotlight className="-top-40 md:left-60 md:top-70" fill="white" />
       {/* <div className="absolute inset-0 -z-10 h-full w-full dark:bg-black dark:bg-[radial-gradient(#000_1px,#000px)]  dark:bg-[size:20px_20px] bg-[radial-gradient(#444649_1px,transparent_1px)] [background-size:16px_16px]" /> */}
       <div className=" w-full text-center p-4   relative">
         <h1 className="mt-20 mb-6 text-6xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50">
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "../lib/utils";
 import Link from "next/link";
-import { Spotlight } from "./ui/Spotlight";
+
 
 function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
@@ -15,7 +15,6 @@ function Navbar({ className }: { className?: string }) {
         className
       )}
     >
-      <Spotlight className="-top-40 md:left-60 md:top-70" fill="white" />
       <Menu setActive={setActive}>
         <Link href="/">
           <MenuItem setActive={setActive} active={active} item="Home">
